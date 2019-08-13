a80ba514d9dac3dbdbd23b7683216f0a45dc2bde
Frank Lyder Bredland <fl@nyggen.com>
2019-08-13 10:43:20 +0200

    Starting to refactor presentation code

--- src/index.js
+++ src/index.js
@@ -1,15 +1,75 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { Provider } from 'react-redux';
-import configureStore from './store';
+import React, { useEffect, cloneElement } from 'react';
+import * as ducks from './presentation.ducks';
+export { ducks };
 
-const store = configureStore();
+//import './presentation.css';
 
-ReactDOM.render(
-	<Provider store={store}>
-		<App />
-	</Provider>,
-	document.getElementById('root')
-);
+import {
+	nextSlideset,
+	prevSlideset,
+	nextSlide,
+	prevSlide,
+	fullscreen,
+	exitFullscreen,
+	jumpToStart,
+} from './presentation.ducks';
+
+const keyCodes = {
+	ArrowDown: 40,
+	ArrowRight: 39,
+	ArrowUp: 38,
+	ArrowLeft: 37,
+	Escape: 27,
+	KeyF: 70,
+	Digit0: 48,
+}
+
+export const defaultKeyMapping = [
+	{	action: nextSlideset,   key: keyCodes.ArrowRight  },
+	{  action: prevSlideset,   key: keyCodes.ArrowLeft   },
+	{  action: nextSlide,      key: keyCodes.ArrowDown   },
+	{  action: prevSlide,      key: keyCodes.ArrowUp     },
+	{  action: fullscreen,     key: keyCodes.KeyF,      ctrlKey: true },
+	{  action: exitFullscreen, key: keyCodes.Escape      },
+	{  action: jumpToStart,    key: keyCodes.Digit0      },
+];
+
+export const Presentation = ({ children, dispatch, keyMappings = defaultKeyMapping }) => {
+	useEffect(() => {
+		function listener(event) {
+			const ret = keyMappings
+				.find( ({ key, ctrlKey = false }) =>
+					key === event.keyCode && ctrlKey === event.ctrlKey
+				);
+
+			if(typeof ret !== 'undefined' && typeof ret.action !== 'undefined') {
+				dispatch(ret.action());
+			}
+		}
+
+		document.addEventListener('keyup', listener);
+		
+		return () => {
+			document.removeEventListener('keyup', listener);
+		};
+	});
+
+	return <div className="presentation">
+		{ children }
+	</div>;
+};
+
+export const Slideset = ({ children, i, slideset }) => {
+	return <div className="slideset" style={{ display: i === slideset ? 'block' : 'none' }}>
+	{
+      React.Children
+           .toArray(children)
+           .map((child, i) => cloneElement(child, { key: i, i }))
+	}
+	</div>;
+};
+
+export const Slide = ({ children, i, slide }) =>
+	<div className="slide" style={{ display: i === slide ? 'block' : 'none' }}>
+	{ children }
+	</div>;
