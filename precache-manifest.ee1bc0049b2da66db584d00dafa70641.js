10c9a39d4af7dfb350f2fb389ef718cabdfa98c5
Frank Lyder Bredland <fl@nyggen.com>
2019-08-13 12:52:35 +0200

    Updates

---
+++ precache-manifest.ee1bc0049b2da66db584d00dafa70641.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "715d9df4686cd3001c95785bcac905ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85c164607f1520446722",
+    "url": "/static/css/main.5cebe70d.chunk.css"
+  },
+  {
+    "revision": "dd11724844482d619ba6",
+    "url": "/static/js/2.7906c956.chunk.js"
+  },
+  {
+    "revision": "85c164607f1520446722",
+    "url": "/static/js/main.7d729aa8.chunk.js"
+  },
+  {
+    "revision": "80c7fbe4e7f8085bbb63",
+    "url": "/static/js/runtime~main.aa9f7e9f.js"
+  }
+]);
